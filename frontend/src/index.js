363c70dde4598d52cda04b820ad21ec4ac04d23d
[arefnasser] <[aref.nasser.eam.js@tuwaiq.edu.sa]>
2021-11-24 09:24:41 +0300

    signingIn

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Router, Route} from 'react-router-dom';
 
 // import signUp from './components/registaration/signUp';
 import App from './App';
 import Navigation from './components/navigation/nav';
 import Header from './components/header/head';
+import SignIn from './components/signInUp/s7ignIn';
 
 // import provider
 // import store
 
 ReactDOM.render(
   // The provider will enable the child components to access the store
-	<Router>
+	<BrowserRouter>
 		<Navigation/>
 		<Header />
+		<SignIn />
 		<App />
-		
-	</Router>,
+	</BrowserRouter>,
   document.getElementById("root")
 );
