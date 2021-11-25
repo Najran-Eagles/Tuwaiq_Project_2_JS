089aded0f4a672a7ab49195cfa19500e0921f830
ArefNasser <92294188+ArefNasser@users.noreply.github.com>
2021-11-25 11:32:03 +0300

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
-// import SignUp from "./components/signInUp/SignUp";
-// import Footer from "./components/footer/footer";
-import Header from "./components/header/head";
-import Navigation from "./components/navigation/nav";
-import SignIn from "./components/signInUp/SignIn";
+import React from 'react';
 // import { Route } from 'react-router-dom';
 
-export default function App() {
-  return (
-    <div>
-      {/* <Navigation /> */}
-	  <Header />
-	  <SignIn />
 
-    </div>
-  );
-}
+const App = () => {
+	return <div className="App">
+		<form>
+
+                
+
+
+</form>
+
+	</div>;
+
+
+
+
+
+};
+
+export default App;
