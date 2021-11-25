e237251f4fbf6ee82d4c2f8bdfac604323f1b878
ArefNasser <92294188+ArefNasser@users.noreply.github.com>
2021-11-25 18:32:18 +0300

    siginingUpandIN

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Router, Route} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 // import signUp from './components/registaration/signUp';
 import App from './App';
 
-import Navigation from './components/navigation/nav';
-import Header from './components/header/head';
-import SignIn from './components/signInUp/s7ignIn';
-import Footer from './components/footer/footer';
+
 
 // import provider
 // import store
@@ -17,11 +14,7 @@ ReactDOM.render(
   // The provider will enable the child components to access the store
 
 	<BrowserRouter>
-		<Navigation/>
-		<Header />
-
-		<SignIn />
-		<Footer />
+	
 		<App />
 	</BrowserRouter>,
   document.getElementById("root")
